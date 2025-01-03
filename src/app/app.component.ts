a522ba04d204b2dc74dd658ef435b6006d24804f
Brian Loftus <BLoftus@brailleworks.com>
2025-01-02 20:54:16 -0500

    Updated Angular version to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import {HomeComponent} from './home/home.component';
 
 @Component({
-  standalone: true,
-  imports: [HomeComponent],
-  selector: 'app-root',
-  template: `
+    imports: [HomeComponent],
+    selector: 'app-root',
+    template: `
     <main>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
@@ -15,7 +14,7 @@ import {HomeComponent} from './home/home.component';
       </section>
     </main>
   `,
-  styleUrls: ['./app.component.css'],
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'homes';
